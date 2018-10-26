6f270d465c886afe372c452add05621e7275b1d2
Joey Cunningham <jc402615@ohio.edu>
2018-10-26 18:53:00 -0400

    Added core files for chrome extension

---
+++ chrome extension core/popup.js
@@ -0,0 +1,14 @@
+var submitButton = document.getElementById("nameButton");
+var automaticButton = document.getElementById("autoButton");
+
+submitButton.addEventListener("click", readInName);
+function readInName(){
+    <!--stub for actual method -->
+    alert("click detected");
+}
+
+automaticButton.addEventListener("click", inPageAuthor);
+function inPageAuthor(){
+    <!--stub for actual method -->
+    alert("automatic author button click detected!");
+}
