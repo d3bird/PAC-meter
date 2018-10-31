5dbdf8c9a10c90ca7994b1da78c9a7935a399088
Joey Cunningham <jc402615@ohio.edu>
2018-10-31 18:40:00 -0400

    Added framework for link hypertext implementation

--- chrome extension core/popup.js
+++ chrome extension core/popup.js
@@ -13,8 +13,6 @@ function readInName(){
 //when user clicks automatic author analysis button
 automaticButton.addEventListener("click", inPageAuthor);
 function inPageAuthor(){
-    //stub for actual method
-    alert("automatic author button click detected!");
 
     //populate array with current active tab
     chrome.tabs.query({"active": true}, function(tabs){
@@ -22,14 +20,36 @@ function inPageAuthor(){
         alert(currentURL + "\n\nThis is a new line");
 
         var rating; // integer level of bias
-        var author; //author of webpage 
-        //links of other pages
-        //var link1;
-        //var link2;
-        //var link3;
+        var author; //author of webpage
+        author;
+        var bias; //numberical value of bias
+
+        //link urls
+        var url1; //these should be set to a default page in the future
+        var url2;
+        var url3;
+
+        //get link urls
+
+        //link elements to other pages
+        var link1 = document.getElementById("link1");
+        link1.setAttribute('href', url1)
+        var link2 = document.getElementById("link2");
+        link2.setAttribute('href', url2);
+        var link3 = document.getElementById("link3");
+        link3.setAttribute('href', url3);
 
         //changing html to results page
         document.getElementById("resultsHeading").innerHTML="Results:";
+        document.getElementById("authorHeading").innerHTML="-Author: " + author;
+        document.getElementById("biasHeading").innerHTML="-Bias: " + bias;
+        document.getElementById("linksHeading").innerHTML="-Other Articles by " + author;
+        document.getElementById("link1").innerHTML="Article 1";
+        document.getElementById("link2").innerHTML="Article 2";
+        document.getElementById("link3").innerHTML="Article 3";
+
+        document.getElementById("main").style.height = "100px";
+
     }
 );
 
