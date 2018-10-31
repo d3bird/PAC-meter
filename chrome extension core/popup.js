1b3ebd7dc97470e9edf6c909282949d27207b21a
Joey Cunningham <jc402615@ohio.edu>
2018-10-31 16:45:49 -0400

    added url fetching method & added core of results page

--- chrome extension core/popup.js
+++ chrome extension core/popup.js
@@ -1,3 +1,5 @@
+//author: Joey Cunningham
+
 //var submitButton = document.getElementById("nameButton");
 var automaticButton = document.getElementById("autoButton");
 
@@ -8,9 +10,26 @@ function readInName(){
     alert("click detected");
 }
 
-//when user clicks automatic author analysis button 
+//when user clicks automatic author analysis button
 automaticButton.addEventListener("click", inPageAuthor);
 function inPageAuthor(){
-    <!--stub for actual method -->
+    //stub for actual method
     alert("automatic author button click detected!");
+
+    //populate array with current active tab
+    chrome.tabs.query({"active": true}, function(tabs){
+        var currentURL = tabs[0].url; //set as current url of tab
+        alert(currentURL + "\n\nThis is a new line");
+
+        var rating; // integer level of bias
+        //links of other pages 
+        //var link1;
+        //var link2;
+        //var link3;
+
+        //changing html to results page
+    }
+);
+
+
 }
