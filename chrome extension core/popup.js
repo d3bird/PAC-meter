415ab2cf2a613501792cad96d2059693e8bd90b3
Scott Crawford <dogbird9@gmail.com>
2018-10-31 23:06:50 -0400

    meerged all js files into one, to advoid errors

--- chrome extension core/popup.js
+++ chrome extension core/popup.js
@@ -1,10 +1,35 @@
-//author: Joey Cunningham
+//author: Joey Cunningham & scott Crawford
+
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+
+var url2 ='https://medium.com/s/story/dmt-is-the-drug-for-our-collective-crisis-of-meaning-eddbb4bb697c';
+
+var art =[];
+
+request(url2, function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    //console.log(html);
+    console.log("running request");
+    var $ = cheerio.load(html);
+    console.log("loading");
+    $("#story, p").each(function(i, elem){//gets the website and adds the article to the art array;
+        art.push($(this).text());
+        //console.log($(this).text());
+    });
+    console.log(art[0]);
+  }//$("h2, div, p")
+});
+
+
 
 //var submitButton = document.getElementById("nameButton");
 var automaticButton = document.getElementById("autoButton");
 
 function getArticle(){
-    return "no";
+    return art;
 }
 
 
