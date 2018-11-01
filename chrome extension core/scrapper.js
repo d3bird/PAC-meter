87e06739942282b01b2e89a5b30411d8fcac02cc
Scott Crawford <dogbird9@gmail.com>
2018-10-31 20:49:47 -0400

    tuned the webscrapper to grab less data, still gets some junk data

--- chrome extension core/scrapper.js
+++ chrome extension core/scrapper.js
@@ -5,22 +5,23 @@ var art =[];
 var title ="could not find";
 var author = "could not find";
 
+var url ='https://www.nytimes.com/2018/10/31/world/middleeast/saudi-arabia-yemen-cease-fire.html?rref=collection%2Fbyline%2Fgardiner-harris&action=click&contentCollection=undefined&region=stream&module=stream_unit&version=latest&contentPlacement=1&pgtype=collection';
 
+function setURL(u){
+	url =u;
+}
 
-request('https://www.nytimes.com/2018/10/31/world/middleeast/saudi-arabia-yemen-cease-fire.html?rref=collection%2Fbyline%2Fgardiner-harris&action=click&contentCollection=undefined&region=stream&module=stream_unit&version=latest&contentPlacement=1&pgtype=collection', function (error, response, html) {
+request(url, function (error, response, html) {
   if (!error && response.statusCode == 200) {
     //console.log(html);
     console.log("running request");
  	var $ = cheerio.load(html);
  	console.log("loading");
- 	$("h2, div, p").each(function(i, elem){
+ 	$("#story, p").each(function(i, elem){//gets the website and adds the article to the art array;
  		art.push($(this).text());
- 		console.log($(this).text());
+ 		//console.log($(this).text());
  	});
-    /*$('p.css-1xl4flh e2kc3sl0').each(function(i, element){
-      var a = $(this).prev();
-      console.log(a.text());
-        });*/
+ 	console.log(art[0]);
   }//$("h2, div, p")
 });
 
