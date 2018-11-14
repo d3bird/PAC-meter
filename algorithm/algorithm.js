0439bdeade8cbc8c2cd7a1eff1632ce7411b8cd7
Jesse Runner <jrunner@odd03.cs.ohiou.edu>
2018-11-14 16:33:37 -0500

    Changed while loop condition and fixed documentation

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -7,7 +7,7 @@ function parse(web_scraper variable){
 	var y = string_array.length - 1;
 	var i = 0;
 	
-	while( i != y){
+	while( i <= y){
 		var samplestr = string_Array[i];
 		if(samplestr[samplestr.length - 1] == "?"){
 			string_array[i] = samplestr.substr(0, samplestr.length - 1); // if the string contains a question mark, it will be removed for accurate comparison of keyword
@@ -17,9 +17,8 @@ function parse(web_scraper variable){
                 }
 		if(samplestr[samplestr.length - 1] == ","){
                         string_array[i] = samplestr.substr(0, samplestr.length - 1); // if the string contains a comma, it will be removed for accurate comparison of keyword
-                }
-		// same will be done for periods, commas, and quotations.
-		compare(string_array[i]); // this sends each individual string into the comparison function,  but this will be changed to return the entire array of characters. It will be left as is for now.
+                }.
+		compare(string_array[i]); // this sends each individual string into the comparison function,  but this will be changed to return the entire array of strings. It will be left as is for now.
 		i++;
 	}
 
