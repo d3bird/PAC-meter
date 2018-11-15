f248a72c92b3410864f59ef75ce0b455020473ca
Scott Crawford <dogbird9@gmail.com>
2018-11-14 21:46:05 -0500

    vastly improved websrapper

--- chrome extension core/popup.js
+++ chrome extension core/popup.js
@@ -5,32 +5,34 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 
-//var url2 ='https://medium.com/s/story/dmt-is-the-drug-for-our-collective-crisis-of-meaning-eddbb4bb697c';
+
 
 var art =[];
 
-//request(url2, function (error, response, html) {
-//  if (!error && response.statusCode == 200) {
-//    //console.log(html);
-//    console.log("running request");
-//    var $ = cheerio.load(html);
-//    console.log("loading");
-//    $("#story, p").each(function(i, elem){//gets the website and adds the article to the art array;
-//        art.push($(this).text());
-//        //console.log($(this).text());
-//    });
-//    console.log(art[0]);
-//  }//$("h2, div, p")
-//});
+function getBais() {
+    var output = 0;
+
+
+
+    return output;
+}
+
 
 
 //var submitButton = document.getElementById("nameButton");
 var automaticButton = document.getElementById("autoButton");
 
-function getArticle(){
-    return art;
-}
 
+function FindAuthor() {
+    var output = "not found";
+    var words;
+    for (var i = 0; i < 2; i++) {
+        words = art[i].split(" ");
+        console.log(words.length);
+    }
+
+    return output;
+}
 
 
 //button for manual author name
@@ -57,11 +59,22 @@ function inPageAuthor(){
                 console.log("running request");
                 var $ = cheerio.load(html);
                 console.log("loading");
-                $("#story, p").each(function (i, elem) {//gets the website and adds the article to the art array;
+
+                //old way of getting the article
+                /*$("#story, p").each(function (i, elem) {//gets the website and adds the article to the art array;
                     art.push($(this).text());
-                    //console.log($(this).text());
+                    console.log($(this).text());
+                    console.log();
+                });*/
+                //new way  of getting the article
+
+                console.log("the article was created by");
+                $("#author creator, p").each(function (i, elem) {//gets the website and adds the article to the art array;
+                     art.push($(this).text());
+                    console.log($(this).text());
+                    console.log();
                 });
-                console.log(art[0]);
+                //console.log(art[0]);
             }//$("h2, div, p")
         });
        
@@ -76,7 +89,7 @@ function inPageAuthor(){
         //code to find author of webpage
 
 
-        var bias; //numberical value of bias
+        var bias = getBais(); //numberical value of bias
 
         //code to calculate bias of author on webpage
 
